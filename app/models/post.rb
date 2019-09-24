e869dddf9f72365f410b7721cfae1040d2b6512e
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-09-24 21:01:43 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,33 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :comments
 
+  def unique_user_comments
+    user_comments = {}
+    self.comments.each do |comment|
+      if user_comments.key?(comment.user)
+        user_comments[comment.user] << comment.content
+      else
+        user_comments[comment.user] = [comment.content]
+      end
+    end
+    user_comments
+  end
 
+  def category_ids=(ids)
+    ids.each do |id|
+      if id != ""
+        category = Category.find(id)
+        self.categories << category
+      end
+    end
+  end
+
+  def categories_attributes=(categories_attributes)
+    categories_attributes.values.each do |category_attr|
+      category = Category.find_or_create_by(category_attr)
+      self.post_categories.build(category: category)
+    end
+  end
 end
