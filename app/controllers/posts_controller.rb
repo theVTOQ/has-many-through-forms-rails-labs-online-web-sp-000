a01f3a400944fea8b5330db0f625527c46c2db62
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-10-01 15:06:01 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,9 +16,43 @@ class PostsController < ApplicationController
     redirect_to post
   end
 
+  def update
+    post = Post.find(params[:id])
+
+    if !params[:comment].empty?
+      content = params[:comment][:content]
+      user_id = params[:comment][:user_id]
+      username = params[:comment][:user_attributes][:username]
+
+      comment = Comment.create(content: content)
+      user = nil
+      if !user_id.blank?
+        user = User.find(user_id)
+      elsif !username.blank?
+        user = User.find_or_create_by(username: username)
+      end
+
+      comment.user = user #if user != nil
+      comment.post = post
+      comment.save
+      post.save
+      #binding.pry
+    else
+
+    end
+    #as yet, we are not editing the data of the post, only creating a comment
+    #for the post
+    #post.update!(post_params)
+    redirect_to post
+  end
+
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, :user_id, category_ids:[], categories_attributes: [:name])
+    params.require(:post).permit(
+      :title, :content, :user_id, category_ids:[],
+      categories_attributes: [:name],
+      comment_attributes: [:content, :user_id, user_attributes: [:username]]
+    )
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,13 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
-
+  accepts_nested_attributes_for :user, reject_if: proc {|attrs| attrs['user'].blank? }
+=begin
+  def user_attributes=(user_attributes)
+    user_attributes.values.each do |user_attr|
+      user = User.find_or_create_by(user_attr)
+      self.user = user
+    end
+  end
+=end
 end
